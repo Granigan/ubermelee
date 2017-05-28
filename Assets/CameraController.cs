3e48655845c4881849a3896035fbbe985f023005
DESKTOP-TEMPPIKONE\Testi <janne.berggren@gmail.com>
2017-05-28 16:56:52 +0300

    Added Camera Controls (zoom and movement). Added particle explosion for ships.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,9 +5,10 @@ public class CameraController : MonoBehaviour
 {
 
     public GameObject player;       //Public variable to store a reference to the player game object
-
+    public GameObject playerPrefab;
 
     private Vector3 offset;         //Private variable to store the offset distance between the player and camera
+    public float respawnTimer;
 
     // Use this for initialization
     void Start()
@@ -19,7 +20,26 @@ public class CameraController : MonoBehaviour
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
-        // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        if(player != null)
+        {
+            // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
+            transform.position = player.transform.position + offset;
+
+        } else
+        {
+            // Player is dead! Time to respawn
+            respawnTimer += Time.deltaTime;
+            if (respawnTimer > 2)
+            {
+                //Action
+                respawnTimer = 0;
+                Quaternion quat = new Quaternion();
+                quat.Set(180f, 0f, 0f, 0f);
+                
+                player = Instantiate(playerPrefab, new Vector3(transform.position.x, transform.position.y, -3.5f), quat);
+                
+            }
+        }
+
     }
 }
