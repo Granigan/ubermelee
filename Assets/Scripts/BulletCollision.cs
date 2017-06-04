807ea55bf1794f20362b9d1b8094e0f14c03d40f
JANNE_LAPTOP\Janne Berggren <janne.berggren@gmail.com>
2017-06-04 10:00:54 +0300

    Added bullet (small) explosion. Added handling for bullet collision and damage to ship. Cleaned up some obsolete objects. Added ship respawn in random location.

---
+++ Assets/Scripts/BulletCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour {
+
+    public float timeToLive = 3.0f;
+    public float damage = 2.0f;
+    public GameObject explosionPrefab;
+
+    // Use this for initialization
+    void Start () {
+        Destroy(this.gameObject, timeToLive);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        //Debug.Log("OnCollisionEnter");
+        if (col.gameObject.tag == "CameraObject")
+        {
+
+            var damageFunction = col.gameObject.GetComponent<ShipHandling>();
+            damageFunction.DoDamage(damage);
+            GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(explosion, 3.0f);
+            Destroy(this.gameObject);
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/ShipHandling.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipHandling : MonoBehaviour {
+
+    public float hitPoints = 20;
+    public GameObject explosionPrefab;
+    public float respawnVariance = 30f;
+
+    private IEnumerator coroutine;
+    private float currHitPoints = 20;
+
+    // Use this for initialization
+    void Start () {
+        currHitPoints = hitPoints;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void DoDamage(float Damage)
+    {
+        this.currHitPoints = this.currHitPoints - Damage;
+        Debug.Log("Hitpoints left " + this.currHitPoints);
+        if(this.currHitPoints <= 0)
+        {
+            explodeShip();
+            currHitPoints = hitPoints;
+        }
+
+    }
+
+    private void explodeShip() { 
+    
+        Debug.Log("Ship Explosion!!!");
+        GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        Destroy(explosion, 3.0f);
+        StartCoroutine(DieAndRespawn());
+    }
+
+
+    private IEnumerator DieAndRespawn()
+    {
+        Debug.Log("Player just died!!");
+        //this.gameObject.SetActive(false);
+        //GetComponent(Rigidbody).enabled = false;
+        this.GetComponentInChildren<MeshRenderer>().enabled = false;
+        this.GetComponentInChildren<Transform>().localScale = new Vector3(0, 0, 0);
+        yield return new WaitForSeconds(2.0f);
+        Debug.Log("Player just respawned!!");
+        //transform.position = new Vector3(0.04833326f, 3.980667f, 0.0f);
+        //transform.rotation = Quaternion.identity;
+        //GetComponent<Renderer>().enabled = true;
+        Vector3 respawnPoint = new Vector3(Random.Range(-respawnVariance, respawnVariance), Random.Range(-respawnVariance, respawnVariance), -3);  // Fix up the static -3 Z-axis later?
+        //this.gameObject.SetActive(true);
+        this.GetComponentInChildren<Transform>().position = respawnPoint;
+        this.GetComponentInChildren<MeshRenderer>().enabled = true;
+        this.GetComponentInChildren<Transform>().localScale = new Vector3(1, 1, 1);
+    }
+}
