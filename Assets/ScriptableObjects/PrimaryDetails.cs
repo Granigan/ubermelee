df76d180e74b1724a0b3fee94f39fd7e331e21b5
Läppäri-Tapio <teromarkustapio@gmail.com>
2017-06-24 19:51:47 +0300

    Added HitPoints for Primary and Secondary. Adjusted default values accordingly.

--- Assets/ScriptableObjects/PrimaryDetails.cs
+++ Assets/ScriptableObjects/PrimaryDetails.cs
@@ -11,9 +11,10 @@ public class PrimaryDetails : PrimarySO {
     public float TimeToLive = 1f;
     public float Mass = 1f;
     public float Drag = 1f;
-    public float AngularDrag = 1f;
-    public float BatteryCharge = 1f;
+    public float AngularDrag = 0f;
+    public float BatteryCharge = 0f;
     public float FireRate = 1f; // How often the weapon can be shot. 1f = Once per second.
+    public float HitPoints = 1f;
     public GameObject bulletPrefab;
     public GameObject explosionPrefab;
 }
--- Assets/ScriptableObjects/SecondaryDetails.cs
+++ Assets/ScriptableObjects/SecondaryDetails.cs
@@ -10,10 +10,11 @@ public class SecondaryDetails : SecondarySO {
     public float Speed = 1f;
     public float TimeToLive = 1f;
     public float Mass = 1f;
-    public float Drag = 1f;
-    public float AngularDrag = 1f;
+    public float Drag = 0f;
+    public float AngularDrag = 0f;
     public float BatteryCharge = 1f;
     public float FireRate = 1f; // How often the special can be used. 1f = Once per second.
+    public float HitPoints = 1f;
     public GameObject specialPrefab;
     
 }
