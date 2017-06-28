7ccdeb125710cf858f256da9380244b6470db090
JANNE_LAPTOP\Janne Berggren <janne.berggren@gmail.com>
2017-06-28 20:22:10 +0300

    Fixed jumping camera. Camera now smooth as a baby's behind!!!

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,15 +2,12 @@
 
 public class CameraControl : MonoBehaviour
 {
-    public float m_DampTime = 0.2f;                 // Approximate time for the camera to refocus.
-    public float m_ScreenEdgeBuffer = 4f;           // Space between the top/bottom most target and the screen edge.
-    public float m_MinSize = 6.5f;                  // The smallest orthographic size the camera can be.
+    public float dampTime = 0.2f;                 // Approximate time for the camera to refocus.
+    public float screenEdgeBuffer = 4f;           // Space between the top/bottom most target and the screen edge.
+    public float minSize = 6.5f;                  // The smallest orthographic size the camera can be.
                                                     //[HideInInspector] public Transform[] m_Targets; // All the targets the camera needs to encompass.
-    public GameObject[] m_Targets; // All the targets the camera needs to encompass.
+    public GameObject[] targets; // All the targets the camera needs to encompass.
 
-    public float maxDistanceX = 16f;
-    public float maxDistanceY = 9f;
-    public float moveOffset = 5f;
     public float maxZoomLevel = 16f;
 
     private Camera mainCamera;                        // Used for referencing the camera.
@@ -18,7 +15,10 @@ public class CameraControl : MonoBehaviour
     private Vector3 moveVelocity;                 // Reference velocity for the smooth damping of the position.
     private Vector3 desiredPosition;              // The position the camera is moving towards.
 
-
+    private float cameraLockedDuration = 0f;
+    public float cameraLockedAfterJump = 1.0f;
+    public float viewpointJumpLeftBottom = 1.2f;
+    public float viewpointJumpRightTop = -0.2f;
 
     private void Awake()
     {
@@ -28,15 +28,22 @@ public class CameraControl : MonoBehaviour
 
     private void FixedUpdate()
     {
-        m_Targets = GameObject.FindGameObjectsWithTag("CameraObject");
+        targets = GameObject.FindGameObjectsWithTag("CameraObject");
 
         // Move the camera towards a desired position.
         Move();
-
         // Change the size of the camera based.
         Zoom();
 
+            
         CheckPlayAreaBoundaries();
+
+        
+
+
+
+        cameraLockedDuration -= Time.fixedDeltaTime;
+        if (cameraLockedDuration < 0f) cameraLockedDuration = 0f;
     }
 
 
@@ -46,7 +53,7 @@ public class CameraControl : MonoBehaviour
         FindAveragePosition();
 
         // Smoothly transition to that position.
-        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref moveVelocity, m_DampTime);
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref moveVelocity, dampTime);
     }
 
 
@@ -56,14 +63,14 @@ public class CameraControl : MonoBehaviour
         int numTargets = 0;
 
         // Go through all the targets and add their positions together.
-        for (int i = 0; i < m_Targets.Length; i++)
+        for (int i = 0; i < targets.Length; i++)
         {
             // If the target isn't active, go on to the next one.
-            if (!m_Targets[i].gameObject.activeSelf)
+            if (!targets[i].gameObject.activeSelf)
                 continue;
 
             // Add to the average and increment the number of targets in the average.
-            averagePos += m_Targets[i].transform.position;
+            averagePos += targets[i].transform.position;
             numTargets++;
         }
 
@@ -71,8 +78,6 @@ public class CameraControl : MonoBehaviour
         if (numTargets > 0)
             averagePos /= numTargets;
 
-        // Keep the same y value.
-        //averagePos.y = transform.position.y;
 
         // The desired position is the average position;
         desiredPosition = averagePos;
@@ -84,16 +89,13 @@ public class CameraControl : MonoBehaviour
         // Find the required size based on the desired position and smoothly transition to that size.
         float requiredSize = FindRequiredSize();
 
-        //if (requiredSize > 5) {
-        requiredSize = requiredSize * 1.5f;
-        //}
 
         if(requiredSize > maxZoomLevel )
         {
             requiredSize = maxZoomLevel;
         }
 
-        mainCamera.orthographicSize = Mathf.SmoothDamp(mainCamera.orthographicSize, requiredSize, ref zoomSpeed, m_DampTime);
+        mainCamera.orthographicSize = Mathf.SmoothDamp(mainCamera.orthographicSize, requiredSize, ref zoomSpeed, dampTime);
         //Debug.Log(mainCamera.orthographicSize);
     }
 
@@ -107,14 +109,14 @@ public class CameraControl : MonoBehaviour
         float size = 0f;
 
         // Go through all the targets...
-        for (int i = 0; i < m_Targets.Length; i++)
+        for (int i = 0; i < targets.Length; i++)
         {
             // ... and if they aren't active continue on to the next target.
-            if (!m_Targets[i].gameObject.activeSelf)
+            if (!targets[i].gameObject.activeSelf)
                 continue;
 
             // Otherwise, find the position of the target in the camera's local space.
-            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].transform.position);
+            Vector3 targetLocalPos = transform.InverseTransformPoint(targets[i].transform.position);
 
             // Find the position of the target from the desired position of the camera's local space.
             Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
@@ -128,10 +130,10 @@ public class CameraControl : MonoBehaviour
         }
 
         // Add the edge buffer to the size.
-        size += m_ScreenEdgeBuffer;
+        size += screenEdgeBuffer;
 
         // Make sure the camera's size isn't below the minimum.
-        size = Mathf.Max(size, m_MinSize);
+        size = Mathf.Max(size, minSize);
 
         return size;
     }
@@ -151,118 +153,79 @@ public class CameraControl : MonoBehaviour
 
     public bool CheckPlayAreaBoundaries()
     {
-        foreach (GameObject currObject in m_Targets)
+        foreach (GameObject currObject in targets)
         {
-            foreach (GameObject otherObject in m_Targets)
-            {
-                if(otherObject == currObject)
-                {
-                    continue;
-                }
+            currObject.GetComponentInChildren<TrailRenderer>().enabled = true;
 
-                Vector3 pos1 = currObject.transform.position;
-                Vector3 pos2 = otherObject.transform.position;
-                pos1.y = 0;
-                pos2.y = 0;
-
-                float distanceX = Vector3.Distance(pos1, pos2);
-
-                if (distanceX > maxDistanceX)
-                {
-                    float newPositionX; // = currObject.transform.position.x + (maxDistanceX * 1) - moveOffset;
-
-                    if (otherObject.transform.position.x > currObject.transform.position.x)
-                    {
-                        newPositionX = currObject.transform.position.x + (maxDistanceX * 2) - moveOffset;
-                    }
-                    else
-                    {
-                        newPositionX = currObject.transform.position.x + ((maxDistanceX * 2 - moveOffset) * -1);
-                    }
-                    //Debug.Log("Before X: "+ currObject.transform.position);
-                    currObject.transform.position = new Vector3(newPositionX, currObject.transform.position.y, currObject.transform.position.z);
-                    //Debug.Log("After X: " + currObject.transform.position);
-                    return true;
-                }
-
-
-
-
-
-                Vector3 pos1y = currObject.transform.position;
-                Vector3 pos2y = otherObject.transform.position;
-                pos1y.x = 0;
-                pos2y.x = 0;
-
-                float distanceY = Vector3.Distance(pos1y, pos2y);
-
-                if (distanceY > maxDistanceY)
-                {
-                    float newPositionY; 
-
-                    if (otherObject.transform.position.y > currObject.transform.position.y)
-                    {
-                        newPositionY = currObject.transform.position.y + (maxDistanceY * 2) - moveOffset;
-                    }
-                    else
-                    {
-                        newPositionY = currObject.transform.position.y + ((maxDistanceY * 2 - moveOffset) * -1);
-                    }
-                    //Debug.Log("Before Y: " + currObject.transform.position);
-                    currObject.transform.position = new Vector3(currObject.transform.position.x, newPositionY, currObject.transform.position.z);
-                    //Debug.Log("After Y: " + currObject.transform.position);
-                    return true;
-                }
-
-
-
-            }
-
+            
             Vector3 viewPos = mainCamera.WorldToViewportPoint(currObject.GetComponentInChildren<Transform>().position);
 
             //Debug.Log("viewPos = " + viewPos);
-            
+            //Debug.Log("currObject.name " + currObject.name);
+
             if (viewPos.x < 0.0f)
             {
-                float newPositionX;
-                newPositionX = currObject.transform.position.x + ((maxDistanceX * 1f - moveOffset) * 1);
-                currObject.transform.position = new Vector3(newPositionX, currObject.transform.position.y, currObject.transform.position.z);
-                if (currObject.name == "ship31") Debug.Log("viewPosX <= 0.0f = " + viewPos);
-                SetStartPositionAndSize();
+                currObject.GetComponentInChildren<TrailRenderer>().enabled = false;
+                Vector3 newLocation = new Vector3(viewpointJumpLeftBottom, viewPos.y, mainCamera.nearClipPlane);
+                newLocation = mainCamera.ViewportToWorldPoint(newLocation);
+
+                currObject.transform.position = new Vector3(newLocation.x, currObject.transform.position.y, currObject.transform.position.z);
+                //Debug.Log("Old = " + viewPos.x + " New = " + newLocation.x);
+                //SetStartPositionAndSize();
+                //lockCameraZoom(cameraLockedAfterJump);
                 return true;
+                
             }
             else if (viewPos.x > 1.0f)
             {
-                float newPositionX;
-                newPositionX = currObject.transform.position.x + ((maxDistanceX * 1f - moveOffset) * -1);
-                currObject.transform.position = new Vector3(newPositionX, currObject.transform.position.y, currObject.transform.position.z);
-                if (currObject.name == "ship31") Debug.Log("viewPosX >= 1.0f = " + viewPos);
-                SetStartPositionAndSize();
+                currObject.GetComponentInChildren<TrailRenderer>().enabled = false;
+                Vector3 newLocation = new Vector3(viewpointJumpRightTop, viewPos.y, mainCamera.nearClipPlane);
+                newLocation = mainCamera.ViewportToWorldPoint(newLocation);
+
+                currObject.transform.position = new Vector3(newLocation.x, currObject.transform.position.y, currObject.transform.position.z);
+                //Debug.Log("Old = " + viewPos.x + " New = " + newLocation.x);
+                //SetStartPositionAndSize();
+                //lockCameraZoom(cameraLockedAfterJump);
                 return true;
+                
             }
 
 
             if (viewPos.y < 0.0f)
             {
-                float newPositionY;
-                newPositionY = currObject.transform.position.y + ((maxDistanceY * 1f - moveOffset) * 1);
-                currObject.transform.position = new Vector3(currObject.transform.position.x, newPositionY, currObject.transform.position.z);
-                if(currObject.name == "ship31") Debug.Log("viewPosY <= 0.0f = " + viewPos);
-                SetStartPositionAndSize();
+                currObject.GetComponentInChildren<TrailRenderer>().enabled = false;
+                Vector3 newLocation = new Vector3(viewPos.x, viewpointJumpLeftBottom, mainCamera.nearClipPlane);
+                newLocation = mainCamera.ViewportToWorldPoint(newLocation);
+
+                currObject.transform.position = new Vector3(currObject.transform.position.x, newLocation.y, currObject.transform.position.z);
+                //Debug.Log("Old = " + viewPos.x + " New = " + newLocation.x);
+                //SetStartPositionAndSize();                
+                //lockCameraZoom(cameraLockedAfterJump);
                 return true;
+                
             } else if (viewPos.y > 1.0f)
             {
-                float newPositionY;
-                newPositionY = currObject.transform.position.y + ((maxDistanceY * 1f - moveOffset) * -1);
-                currObject.transform.position = new Vector3(currObject.transform.position.x, newPositionY, currObject.transform.position.z);
-                if (currObject.name == "ship31") Debug.Log("viewPosY >= 1.0f = " + viewPos);
-                SetStartPositionAndSize();
+                currObject.GetComponentInChildren<TrailRenderer>().enabled = false;
+                Vector3 newLocation = new Vector3(viewPos.x, viewpointJumpRightTop, mainCamera.nearClipPlane);
+                newLocation = mainCamera.ViewportToWorldPoint(newLocation);
+
+                currObject.transform.position = new Vector3(currObject.transform.position.x, newLocation.y, currObject.transform.position.z);
+                //Debug.Log("Old = " + viewPos.x + " New = " + newLocation.x);
+                //SetStartPositionAndSize();
+                //lockCameraZoom(cameraLockedAfterJump);
                 return true;
+                
             }
 
 
         }
         return false;
+    }
+
+    private void lockCameraZoom(float seconds)
+    {
+        cameraLockedDuration += seconds;
+        cameraLockedDuration = Mathf.Min(cameraLockedDuration, 1.0f);
     }
 
 
