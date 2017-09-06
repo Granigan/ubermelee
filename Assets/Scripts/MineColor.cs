2561b3d4e1938de2dbf459d27258636153bc839e
Odin\Antti <antti.ike@gmail.com>
2017-09-06 22:33:15 +0300

    Mines have the same colour ring as the owner

---
+++ Assets/Scripts/MineColor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineColor : MonoBehaviour
+{
+
+    public GameObject[] players;
+    public Color[] ringColor;
+    public GameObject ring;
+    GameObject mine;
+    public int player;
+    public float playerNumber;
+    //public GameObject[] howMany;
+    //public int i;
+    //bool shipDestroyed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        ringColor = new Color[5];
+        ringColor[0] = Color.white;
+        ringColor[1] = Color.cyan;
+        ringColor[2] = Color.red;
+        ringColor[3] = Color.green;
+        ringColor[4] = Color.magenta;
+        rings();
+    }
+
+
+    // Update is called once per frame
+    void rings()
+    {
+        mine = this.gameObject;
+        playerNumber = GetComponent<BulletCollision>().bulletOwnerPlayerNumber;
+        player = (int)playerNumber;
+        GameObject childObject = Instantiate(ring) as GameObject;
+        childObject.transform.parent = mine.transform;
+        childObject.transform.localPosition = Vector3.zero;
+        childObject.GetComponent<Renderer>().material.color = ringColor[player];
+
+    }
+}
+
